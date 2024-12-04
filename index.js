3398360771585313d2d5f30f9eb5f6e1bcbdbe30
daksh042003 <dakshpandey127@gmail.com>
2024-12-04 12:31:13 +0530

    commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const express=require("express");
 const path=require("path");
 const userRoute=require("./routes/user");
+const blogRoute=require("./routes/blog");
+const Blog=require("./models/blog");
 const mongoose=require("mongoose");
 const cookieParser =require("cookie-parser");
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
@@ -17,13 +19,17 @@ app.set("views",path.resolve("./views"));
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser()); 
 app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve('./public')));
 
 
 app.use("/user",userRoute);
+app.use("/blog",blogRoute);
 
-app.get("/",(req,res)=>{
+app.get("/",async(req,res)=>{
+     const allBlogs = await Blog.find({});
      res.render("home",{
           user: req.user,
+          blogs: allBlogs,
      });
 })
 
